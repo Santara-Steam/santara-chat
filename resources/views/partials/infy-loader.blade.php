fa3572f73351d2abef5455186c8e70cd8a489199
Risfandi Bayu <risfandibayu19@gmail.com>
2022-06-20 23:42:35 +0700

    preloader

--- resources/views/partials/infy-loader.blade.php
+++ resources/views/partials/infy-loader.blade.php
@@ -1,4 +1,4 @@
-<svg width="150px" height="75px" viewBox="0 0 187.3 93.7"
+{{-- <svg width="150px" height="75px" viewBox="0 0 187.3 93.7"
      preserveAspectRatio="xMidYMid meet">
     <path stroke="#00c6ff" id="outline" fill="none" stroke-width="5" stroke-linecap="round"
           stroke-linejoin="round" stroke-miterlimit="10"
@@ -7,4 +7,17 @@
           stroke-linecap="round"
           stroke-linejoin="round" stroke-miterlimit="10"
           d="M93.9,46.4c9.3,9.5,13.8,17.9,23.5,17.9s17.5-7.8,17.5-17.5s-7.8-17.6-17.5-17.5c-9.7,0.1-13.3,7.2-22.1,17.1 -8.9,8.8-15.7,17.9-25.4,17.9s-17.5-7.8-17.5-17.5s7.8-17.5,17.5-17.5S86.2,38.6,93.9,46.4z"/>
-</svg>
+</svg> --}}
+<style>
+      .loader {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('https://santara.co.id/public/assets/images/loader.gif') 50% 50% no-repeat rgb(249, 249, 249);
+    opacity: .6;
+  }
+</style>
+<div id="loader" class="loader"></div>
